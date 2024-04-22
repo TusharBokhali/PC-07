b60a2a6e1849310688b51dd9d51baf1c40b49f41
TusharBokhali <variyatushar2023.katargam@gmail.com>
2024-04-22 12:24:33 +0530

    okhj

---
+++ Project1/src/project1/Project1.java
@@ -0,0 +1,29 @@
+
+package project1;
+
+
+public class Project1 {
+
+   
+    public static void main(String[] args) {
+        
+    int a=10;
+    int b=20;
+    int z=a+b;
+    System.out.println("A=>"+a);
+    System.out.println("B=>"+b);
+    System.out.println("sum of="+z);
+    
+    z=0;
+    z=a*b;
+    
+    System.out.println("sum of="+z);
+    
+    z=0;
+    z=z/b;
+    System.out.println("sum of="+z);
+   
+    System.out.println("value=>"+value);
+    }
+    
+}
